c8b98b031f7fd34f084a79a557e280e0845acb20
Moamen Mohamed <moamenmohamedalii@gmail.com>
2020-06-11 18:53:21 +0200

    Initial setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspDotnetMvcToyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
