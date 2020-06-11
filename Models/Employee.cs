67baed2a5f6df62ec755b507ee8f0539fe5991bd
Moamen Mohamed <moamenmohamedalii@gmail.com>
2020-06-11 19:16:06 +0200

    Create Employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotnetMvcToyApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
